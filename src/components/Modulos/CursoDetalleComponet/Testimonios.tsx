c33fa388055b10486d290d2c33c4a39967847fd2
cadicv2 <ezio1622@gmail.com>
2025-03-11 12:40:51 -0500

    fix CursoDetalle

--- src/components/Modulos/CursoDetalleComponet/Testimonios.tsx
+++ src/components/Modulos/CursoDetalleComponet/Testimonios.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Comentarios = () => {
   return (
-    <div className=" rounded-3xl py-10 h-[750px]  bg-[url('https://pub-3d37c601c64a44ff8ec0a62bc03016eb.r2.dev/Ejemplos/bg-Rese%C3%B1a.jpeg')] bg-no-repeat bg-cover bg-center">
+    <div className=" rounded-[90px] py-10 h-[750px]  bg-[url('https://pub-3d37c601c64a44ff8ec0a62bc03016eb.r2.dev/Ejemplos/bg-Rese%C3%B1a.jpeg')] bg-no-repeat bg-cover bg-center">
       <CarruselComentarios />
     </div>
   )
