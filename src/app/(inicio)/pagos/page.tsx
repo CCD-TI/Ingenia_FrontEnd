2f1317d80dbe43195a214f01bd938348c65cdeb6
isabel <olazabalisabel53@gmail.com>
2025-04-09 22:27:07 -0500

    pagos

--- src/app/(inicio)/pagos/page.tsx
+++ src/app/(inicio)/pagos/page.tsx
@@ -2,25 +2,26 @@
 import BeneficiosPagos from "@/components/Modulos/BeneficiosPagos";
 import { FaCheckSquare } from "react-icons/fa";
 import Image from "next/image";
+import { MdKeyboardArrowLeft } from "react-icons/md";
 
 export default function Pagos() {
     return (
         <>
             <div className='bg-[url("/img/testimonios.jpg")] relative w-full h-[900px] bg-cover bg-center flex flex-col items-start 
             justify-center gap-1 '>
-            <div className=" ml-10">Regresar</div>
-            <div className="flex items-start justify-center gap-12 mx-auto max-sm:flex-col max-sm:gap-2"> 
-            <div className="w-[56rem] h-[39rem] border-2 border-cyan-600 p-2 rounded-[30px] bg-white/10">
-            <div className="flex gap-64 mt-3">
-                <h1 className="py-4 pl-16 font-poppins font-semibold text-2xl">Información de pago</h1>
-                <button className="border-2 border-white bg-transparent  px-7 rounded-full font-poppins font-extralight text-lg">Eliminar Item</button>
+            <span className=" ml-16 flex gap-2 items-center"><MdKeyboardArrowLeft className="w-5 h-5" />Regresar</span>
+            <div className="flex gap-10 items-start justify-center  mx-auto mt-4 max-sm:flex-col max-sm:gap-2 "> 
+            <div className="w-[53rem] h-[39rem] border-2 border-cyan-600 py-2 pl-2 pr-3 rounded-[30px] bg-white/10 shadow-[0_30px_50px_42px_rgba(8,145,178,0.3)]  ">
+            <div className="flex gap-52 mt-3">
+                <h1 className="py-4 pl-16 font-poppins font-semibold text-3xl drop-shadow-[0_0_10px_rgba(255,255,255,2)]">Información de pago</h1>
+                <button className="border-2 border-white bg-transparent  px-7 rounded-full font-poppins font-extralight text-lg hover:bg-white hover:text-black">Eliminar Item</button>
             </div>
             <div className=" px-16">
-                    <span className="flex gap-3 mt-3"><FaCheckSquare/>2 Item Seleccionado</span>
+                    <span className="flex gap-3 items-center mt-3"><FaCheckSquare/>2 Item Seleccionado</span>
                     <div className="w-full h-[3px] bg-white drop-shadow-[0_0_10px_rgba(255,255,255,2)] mt-2 "></div>
                 </div>
-
-                <div className="flex items-center gap-4 pl-16 mt-6">
+                <div className="max-h-[400px] overflow-y-auto pr-4 mt-4">
+                <div className="flex items-center gap-4 pl-10 mt-6 ">
                     <span className="flex gap-4"><FaCheckSquare className="w-6 h-6"/></span>
                     <div className="h-[120px] w-[120px]">
                           <Image
@@ -28,7 +29,7 @@ export default function Pagos() {
                                           src="/img/ciencias1.jpeg"
                                           width={1000}
                                           height={1000}
-                                          className="w-full h-full object-cover "
+                                          className="w-full h-full object-cover  rounded-xl "
                                         />
                     </div>
                     <span className="flex flex-col "> 
@@ -40,7 +41,7 @@ export default function Pagos() {
                     <span className="ml-40 font-poppins font-semibold text-xl">S/. 599.99</span>
                 </div> 
 
-                <div className="flex items-center gap-4 pl-16 mt-4">
+                <div className="flex items-center gap-4 pl-10 mt-4">
                     <span className="flex gap-4"><FaCheckSquare className="w-6 h-6"/></span>
                     <div className="h-[120px] w-[120px]">
                           <Image
@@ -48,7 +49,7 @@ export default function Pagos() {
                                           src="/img/ciencias1.jpeg"
                                           width={1000}
                                           height={1000}
-                                          className="w-full h-full object-cover "
+                                          className="w-full h-full object-cover  rounded-xl"
                                         />
                     </div>
                     <span className="flex flex-col "> 
@@ -61,7 +62,7 @@ export default function Pagos() {
 
                 </div>
 
-                <div className="flex items-center gap-4 pl-16 mt-4">
+                <div className="flex items-center gap-4 pl-10 mt-4">
                     <span className="flex gap-4"><FaCheckSquare className="w-6 h-6"/></span>
                     <div className="h-[120px] w-[120px] ">
                           <Image
@@ -69,7 +70,7 @@ export default function Pagos() {
                                           src="/img/ciencias1.jpeg"
                                           width={1000}
                                           height={1000}
-                                          className="w-full h-full object-cover "
+                                          className="w-full h-full object-cover rounded-xl"
                                         />
                     </div>
                     <span className="flex flex-col "> 
@@ -86,31 +87,35 @@ export default function Pagos() {
             
             </div>
 
+                </div>
+                
 
 
 
-            <div className="w-[39rem] h-[39rem] border-2 border-cyan-600 pt-8 px-16 rounded-[30px] bg-white/10 ">
-            <h1 className="font-poppins font-semibold text-2xl">Subtotal</h1> 
-           <div className="flex gap-[340px] mt-10">
+
+            <div className="w-[34rem] h-[39rem] border-2 border-cyan-600 pt-8 px-16 rounded-[30px] bg-white/10 shadow-[0_30px_50px_42px_rgba(8,145,178,0.3)]">
+            <h1 className="font-poppins font-semibold text-3xl drop-shadow-[0_0_10px_rgba(255,255,255,2)]">Subtotal</h1> 
+           <div className="flex gap-[259px] mt-10">
             <span className="font-poppins font-extralight">Física</span>
             <span className="font-poppins font-semibold text-xl">S/. 599.99</span>
 
            </div>
 
-           <div className="flex gap-[350px] mt-10">
+           <div className="flex gap-[269px] mt-10">
             <span className="font-poppins font-extralight">Descuento</span>
             <span  className="font-poppins font-semibold text-xl">S/. 0</span>
               
            </div>
            <div className="w-full h-[3px] bg-white drop-shadow-[0_0_10px_rgba(255,255,255,2)] mt-10 "></div>
 
-           <div className="flex gap-[350px] mt-10">
-            <span className="font-poppins font-extralight"> Total</span>
+           <div className="flex gap-[269px] mt-10">
+            <span className="font-poppins font-extralight drop-shadow-[0_0_10px_rgba(255,255,255,2)]"> Total</span>
             <span  className="font-poppins font-semibold text-xl">S/. 599.99</span>
               
            </div>
 
-           <button className="bg-transparent border-2 border-white p-4 rounded-full w-full mt-14">Continuar</button>
+           <button className="bg-transparent border-2 border-white p-4 rounded-full w-full mt-14 drop-shadow-[0_0_30px_rgba(255,255,255,8)] uppercase
+           text-2xl font-poppins font-black hover:text-cyan-600 hover:bg-white">Continuar</button>
                    
             
             </div>
