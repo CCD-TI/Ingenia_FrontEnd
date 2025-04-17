92cbcef1901d565d02ba963dc22ff327f3af1b4e
Paul Damian <pauldamiansarro@gmail.com>
2025-04-17 08:43:55 -0500

    add img dashboard

--- src/components/ui-elements/logo.tsx
+++ src/components/ui-elements/logo.tsx
@@ -6,8 +6,8 @@ export function Logo() {
   return (
     <div className="relative flex justify-center items-center h-25 aspect-w-1 aspect-h-1">
       <Image
-        // src={"/img/logo/Isotipo_vrs color_Digital Collage_2025.jpg"}
-        src={"/img/logo/Isotipo_vrs blanco_Digital Collage_2025.jpg"}
+        src={"/img/logo/Isotipo_vrs color_Digital Collage_2025.jpg"}
+        // src={"/img/logo/Isotipo_vrs blanco_Digital Collage_2025.jpg"}
         fill
         className="dark:hidden object-contain"
         alt="NextAdmin logo"
